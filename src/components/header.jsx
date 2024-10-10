8f147674bb9775c9146a38d6460eb2498528a096
shakhrukhturayev <shakhrukhturaev1998@gmail.com>
2024-10-10 19:54:35 +0500

    Margin edited

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
       className={` ${styles.DisplayBetween} ${styles.PaddingX} flex flex-wrap w-full `}
     id="home">
       <div className="w-full lg:w-1/2">
-        <h1 className="text-[40px] font-bold sm:text-[60px] sm:pt-[300px] leading-[70px] text-[#145464]">
+        <h1 className="text-[40px] font-bold sm:text-[60px] sm:pt-[250px] leading-[70px] text-[#145464]">
           Bionyx.corp <br /> Sog'lig'inggiz ishonchli qo'llarda.
         </h1>
         <p className="mt-10 max-w-[540px] text-[12px]  sm:text-[20px] font-[400] text-[#145464]">
@@ -19,7 +19,7 @@ const Header = () => {
           ishlab chiqarish va tarqatishda eng ilg‘or texnologiyalardan
           foydalanamiz.
         </p>
-        <a href="https://t.me/+998954502282"><Button styles={"mt-20 mb-[95px]"} /></a>
+        <a href="https://t.me/+998954502282" className="mt-10 mb-[95px]"><Button styles={"mt-10 mb-[95px]"} /></a>
       </div>
       <div className="w-full lg:w-1/2 h-full">
         <Carousel />

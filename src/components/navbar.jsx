ed866b511c6ac9e7125ccc3cd6ffc159edcf746b
shakhrukhturayev <shakhrukhturaev1998@gmail.com>
2024-10-11 13:42:15 +0500

    navbar edited animation

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -26,10 +26,10 @@ const Navbar = () => {
                 ))}
             </ul>
             {/* Hamburger menu and Close btns  section*/}
-            <div className='sm:hidden flex flex-1 justify-end items-center'>
+            <div className='sm:hidden flex flex-1 justify-end items-center '>
                 <img src={toggleNav ? Close : menu} alt="no pic" className='w-[35px] h-[35px] object-contain' onClick={toggleHandler} />
                 <div className={`${!toggleNav ? 'hidden' : 'flex'} py-6 absolute top-[58px] right-0 left-0 h-full w-full z-10`}>
-                    <ul className='list-none flex-col items-center justify-center flex-1 w-full bg-slate-50'>
+                    <ul className='list-none flex-col items-center justify-center flex-1 w-full bg-slate-50 duration-500'>
                         {NavLinks.map((nav, idx) => (
                             <li key={nav.id} className={`${idx === NavLinks.length ? 'mt-0' : 'mt-5'} 
                     ${active === nav.id ? 'text-lightBlue' : 'text-darkGreen'} cursor-pointer font-raleway font-semibold text-darkGreen hover:ml-2 text-[18px] transition-all duration-500 border-b-[1px] border-[#8ce404] px-3`}
